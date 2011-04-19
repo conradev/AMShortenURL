6f2b7abce0b85bf28bc9d5c63ab0853e9cb8f5a5
Conrad Kramer <ckrames1234@gmail.com>
2011-04-19 00:16:48 -0400

    Adds GData framework to build. (For Google OAuth for Goo.gl)

--- Makefile
+++ Makefile
@@ -5,15 +5,28 @@
 BUNDLE_NAME = ShortURL
 ShortURL_FILES = AMShortenURL.m AMShortenURLPrefs.m
 ShortURL_FILES += $(shell find Classes/ -iname '*.m')
-ShortURL_FILES += $(shell find json-framework/Classes/ -iname '*.m') 
-ShortURL_CFLAGS = -I./Classes/ -I./json-framework/Classes/
+ShortURL_CFLAGS = -I./Classes/ 
 ShortURL_LDFLAGS = -licucore
 ShortURL_FRAMEWORKS = UIKit SystemConfiguration
 ShortURL_PRIVATE_FRAMEWORKS = Preferences
 ShortURL_INSTALL_PATH = /Library/ActionMenu/Plugins/
 
+# JSON Framework (For Goo.gl)
+ShortURL_FILES += $(shell find json-framework/Classes/ -iname '*.m') 
+ShortURL_CFLAGS += -I./json-framework/Classes/
+
+# GData Framework (For Goo.gl)
+ShortURL_LDFLAGS += -F. -framework GData
+
 include theos/makefiles/common.mk
 include $(THEOS_MAKE_PATH)/bundle.mk
+
+########## Custom Build Rules ##########
+
+ifeq ($(shell [ -d "$(FW_PROJECT_DIR)/GData.framework" ] && echo 1 || echo 0),0)
+before-ShortURL-all::
+	$(ECHO_NOTHING)$(FW_PROJECT_DIR)/get_gdata.sh$(ECHO_END)
+endif
 
 internal-bundle-stage_::
 	$(ECHO_NOTHING)mv "$(FW_SHARED_BUNDLE_RESOURCE_PATH)/$(FW_INSTANCE)" "$(FW_SHARED_BUNDLE_RESOURCE_PATH)/Shorten URL"$(ECHO_END)
---
+++ get_gdata.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Code modified from ashikase's Backgrounder get_requirements.sh script (No apparent license either)
+# https://github.com/ashikase/Backgrounder
+
+# Either wget or curl is needed to download package list and ldid.
+WGET=$(type -P wget)
+CURL=$(type -P curl)
+if [ -z "$WGET" -a -z "$CURL" ]; then
+    echo "ERROR: This script requires either 'wget' or 'curl' to be installed."
+    exit 1
+fi
+
+# Download GData framework
+echo "Downloading GData framework..."
+GDATA_REPO="http://apt.thebigboss.org/repofiles/cydia"
+pkg=""
+if [ ! -z "$WGET" ]; then
+    wget -q "${GDATA_REPO}/dists/stable/main/binary-iphoneos-arm/Packages.bz2"
+    pkg_path=$(bzcat Packages.bz2 | grep "debs2.0/gdata" | awk '{print $2}')
+    pkg=$(basename $pkg_path)
+    wget -q "${GDATA_REPO}/${pkg_path}"
+else
+    curl -s -L "${GDATA_REPO}/dists/stable/main/binary-iphoneos-arm/Packages.bz2" > Packages.bz2
+    pkg_path=$(bzcat Packages.bz2 | grep "debs2.0/gdata" | awk '{print $2}')
+    pkg=$(basename $pkg_path)
+    curl -s -L "${GDATA_REPO}/${pkg_path}" > $pkg
+fi
+ar -p $pkg data.tar.gz | tar --wildcards -zxf - ./System/Library/Frameworks/*
+mv ./System/Library/Frameworks/* .
+rm -rf usr Packages.bz2 $pkg
